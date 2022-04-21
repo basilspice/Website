b2b4d517ff88fa605bc803e714adbc0c8d234c6e
basil <bizzlebreton@gmail.com>
2022-04-21 16:00:16 -0700

    updated homepage

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,14 @@
+import './Footer.css'
+
+const Footer = () => (
+  <footer className='footer'>
+    <a
+      href='https://github.com/basilspice'
+      className='link footer__link'
+    >
+     basilspice
+    </a>
+  </footer>
+)
+
+export default Footer
